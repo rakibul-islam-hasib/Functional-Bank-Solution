d648f9d6527773e957792b34eaeac9d772041236
Hasib Hossain <boraborhasib@gmail.com>
2023-03-08 21:23:43 +0600

    Withdraw Part Fixed

--- js/withdraw.js
+++ js/withdraw.js
@@ -10,11 +10,19 @@
 */
 
 document.getElementById("btn-withdraw").addEventListener("click", function () {
+  // console.log('withdraw btn clicked')
+
   const newWithdrawAmount = getInputFieldValueById("withdraw-field");
-  const previousWithdrawTotal = getTextElementValueById("withdraw-total ");
+
+  const previousWithdrawTotal = getTextElementValueById("withdraw-total");
+
   const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  setTextElementValueById(withdraw - total, newWithdrawTotal);
+
+  setTextElementValueById('withdraw-total', newWithdrawTotal);
+
   const previousBalanceTotal = getTextElementValueById("balance-total");
+
   const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
   setTextElementValueById("balance-total", newBalanceTotal);
 });
